cdf7e761e9ff45d85315ff7c16b954f5082387c5
ErranderLee <dre12am14@gmail.com>
2022-04-03 03:29:42 +0900

    Feat/#41 : - 파일 업로드 기능 - 프로필 테이블에 사진 저장

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -6,6 +6,9 @@ import { User } from './entities/user.entity'
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
 import { Department } from 'src/departments/entities/department.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { diskStorage } from 'multer';
 
 @Module({
   imports: [
@@ -14,6 +17,23 @@ import { Department } from 'src/departments/entities/department.entity';
       Department,
     ]),
     forwardRef(()=>AuthModule),
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        storage: diskStorage({
+          destination: (req, file, cb) => {
+            const dest = `${configService.get('FILE_SAVE_PATH')}`;
+            cb(null, dest);
+          },
+          filename: (req, file, cb) => {
+            const timeStamp = Date.now();
+            const fileName = timeStamp + file.originalname;
+            return cb(null, `${fileName}`);
+          }
+        })
+      })
+    }),
   ],
   controllers: [UserController],
   providers: [UserService],
