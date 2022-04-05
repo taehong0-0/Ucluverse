854ea10d780e3df654cf9d2e17fb5f0ab207fdd1
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-04-06 02:11:40 +0900

    Feat/#59 Board 컴포넌트 구현

---
+++ client/src/Components/Club/Board/ClubBoard.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import { ClubBoardContainer } from './style';
+const ClubBoard = (): ReactElement => {
+  return (
+    <ClubBoardContainer>
+      <span>board</span>
+    </ClubBoardContainer>
+  );
+};
+export default ClubBoard;
