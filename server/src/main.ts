d26e84a361126a9b8e4f97277e9fe3dda9a0fb27
ErranderLee <dre12am14@gmail.com>
2022-04-09 18:09:51 +0900

    CORS 허용

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
