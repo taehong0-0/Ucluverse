a8b20711e809886ee1106790079947528178eb17
ErranderLee <dre12am14@gmail.com>
2022-04-09 17:13:37 +0900

    Feat/#78 : 댓글 생성/수정/삭제 API

--- server/src/comments/comments.controller.ts
+++ server/src/comments/comments.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Post, Query } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+    constructor(
+        private readonly commentsService: CommentsService,
+    ) {}
+
+    @Post()
+    async createComment(@Body() createCommentDto: CreateCommentDto) {
+        return this.commentsService.createComment(createCommentDto);
+    }
+
+    @Patch()
+    async updateComment(@Query('commentIdx') commentIdx: number, @Body() updateCommentDto: UpdateCommentDto) {
+        return this.commentsService.updateComment(commentIdx, updateCommentDto);
+    }
+
+    @Delete()
+    async deleteComment(@Query('commentIdx') commentIdx: number) {
+        return this.commentsService.deleteComment(commentIdx);
+    }
+}
