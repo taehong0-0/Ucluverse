909f21bb475e13f24ec80390e6479a7fade6362a
ErranderLee <dre12am14@gmail.com>
2022-04-12 15:49:53 +0900

    cors 설정 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,12 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
