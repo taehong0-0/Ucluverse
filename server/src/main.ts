3bf2fe5bdcc5caa302b8777832907e48b6a3336b
ErranderLee <dre12am14@gmail.com>
2022-04-14 03:59:33 +0900

    log, cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "*",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
