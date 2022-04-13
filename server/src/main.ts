189d8fd8d3c1ab11769c75bbdfc2df708620b468
ErranderLee <dre12am14@gmail.com>
2022-04-14 04:24:04 +0900

    cors 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
