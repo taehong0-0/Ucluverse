4293cb78fb722cfd23fa59109ddf951d4d6db705
ErranderLee <dre12am14@gmail.com>
2022-04-24 22:48:39 +0900

    Port number .env에서 참조하도록 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
@@ -22,6 +23,7 @@ async function bootstrap() {
       enableImplicitConversion: true,
     },
   }));
-  await app.listen(3000);
+  
+  await app.listen(process.env.PORT);
 }
 bootstrap();
