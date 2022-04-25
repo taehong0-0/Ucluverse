9b0f8ee3d276834578cc7e913d09b99d636724c6
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-04-25 23:21:48 +0900

    Feat/#112 : Posting 컴포넌트 추가 및 글 작성 테스트

--- client/src/Components/Club/Posting/Posting.tsx
+++ client/src/Components/Club/Posting/Posting.tsx
@@ -1,6 +1,26 @@
+import { useRef, useState } from 'react';
+import Button from '../../Button/Button';
+import Editor from '../../Editor/Editor';
+import FloatInput from '../../Input/Input';
 import { PostingContainer } from './style';
 
-const Posting = () => {
-  return <PostingContainer></PostingContainer>;
+interface props {
+  clubId: number;
+  boardIdx: Number;
+}
+const Posting = (props: props) => {
+  const { clubId, boardIdx } = props;
+  const titleRef = useRef(null);
+  const [content, setContent] = useState<string>('');
+  const submit = () => {
+    console.log(content);
+  };
+  return (
+    <PostingContainer>
+      <FloatInput inputRef={titleRef} name="제목" />
+      <Editor setContent={setContent} />
+      <Button name="작성" clickEvent={submit}></Button>
+    </PostingContainer>
+  );
 };
 export default Posting;
--- client/src/Components/Club/Posting/style.ts
+++ client/src/Components/Club/Posting/style.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
+import { BoardStyle } from '../Board/style';
 
-export const PostingContainer = styled.div``;
+export const PostingContainer = styled.div`
+  ${BoardStyle}
+  div:first-child {
+    margin-left: 20px;
+  }
+  button {
+    margin-top: 10px;
+    margin-left: 52vw;
+  }
+`;
