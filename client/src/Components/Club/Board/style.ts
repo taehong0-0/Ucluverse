aa66e91c8614b1f67c477d441350aa2ff347b731
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-04-25 17:40:33 +0900

    Feat/#60 : 동아리 게시판 공통 스타일 추가

--- client/src/Components/Club/Board/style.ts
+++ client/src/Components/Club/Board/style.ts
@@ -3,8 +3,24 @@ import { css } from 'styled-components';
 export const BoardStyle = css`
   margin-left: 20px;
   width: 56.3vw;
+  min-height: 70vh;
+
   border-radius: 0px 0px 5px 5px;
-  overflow: hidden;
   border: 1px solid #dddae0;
-  height: 70vh;
+  height: max-content;
+  .navigator {
+    height: 53px;
+    span:first-child {
+      margin-left: 10px;
+      line-height: 53px;
+      font: normal normal normal 14px/20px Noto Sans KR;
+      letter-spacing: -1.05px;
+      color: #846116;
+      opacity: 1;
+    }
+    span {
+      line-height: 53px;
+      margin-right: 10px;
+    }
+  }
 `;
