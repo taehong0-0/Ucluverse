a5e3513c3a2f5c83b29c995d5a024af121ddc001
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-04-25 23:20:26 +0900

    Feat/#112 : 글 작성 버튼 및 라우팅 처리

--- client/src/Components/Club/Board/PostBoard/style.ts
+++ client/src/Components/Club/Board/PostBoard/style.ts
@@ -3,4 +3,10 @@ import { BoardStyle } from '../style';
 
 export const ClubBoardContainer = styled.div`
   ${BoardStyle}
+  a:last-child {
+    button {
+      margin-top: 20px;
+      margin-left: 52vw;
+    }
+  }
 `;
