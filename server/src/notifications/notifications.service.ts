9e6f6e713d382eaa4d71d232d3654463dce75f1e
Kwondaehwi <kwondh8561@ajou.ac.kr>
2022-04-27 22:45:14 +0900

    Feat/#125 알림 불러오기

--- server/src/notifications/notifications.service.ts
+++ server/src/notifications/notifications.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { NotificationResDto } from './dto/notifcation-response.dto';
+import { Notification } from './entities/notification.entity';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+    constructor(
+        private connection: Connection,
+    ){}
+
+    async getNotifications(userIdx: number){
+        const queryrunner = this.connection.createQueryRunner();
+        const notifications = await queryrunner.manager.find(Notification, {
+            where:{
+                userIdx,
+            }
+        });
+        return new NotificationResDto(notifications);
+    }
+}
