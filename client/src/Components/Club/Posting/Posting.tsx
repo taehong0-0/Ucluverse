034772ed4feb2c43cc8ecf8441d1dc0c956398c6
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-04-28 19:34:19 +0900

    Feat/#122 s3업로드 후 url 변경

--- client/src/Components/Club/Posting/Posting.tsx
+++ client/src/Components/Club/Posting/Posting.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState } from 'react';
-import { isPropertyAccessChain } from 'typescript';
 import Button from '../../Button/Button';
 import Editor from '../../Editor/Editor';
 import FloatInput from '../../Input/Input';
 import { PostingContainer } from './style';
+import { Buffer } from 'buffer';
 import AWS from 'aws-sdk';
 
 interface props {
@@ -14,32 +14,42 @@ const Posting = (props: props) => {
   const { clubId, boardIdx } = props;
   const titleRef = useRef(null);
   const [content, setContent] = useState<string>('');
-  const submit = () => {
+  const submit = async () => {
     const option = {
       accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       region: process.env.REACT_APP_AWS_REGION,
     };
     const s3 = new AWS.S3(option);
-
     const srcRegEx = /<img src=\"([^\"]*?)\" \/>/gi;
     const srcList = content.match(srcRegEx);
-
-    srcList?.forEach(async (tag) => {
+    let idx = 0;
+    var result = content;
+    for (let tag of srcList ?? []) {
+      idx++;
       tag.match(srcRegEx);
+      const srcData = RegExp.$1;
+      const base64Data = Buffer.from(
+        srcData.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+      );
+      const type = srcData.split(';')[0].split('/')[1];
+
       const param = {
         Bucket: process.env.REACT_APP_AWS_S3_BUCKET_NAME ?? '',
         ACL: 'public-read',
-        ContentType: 'image/png',
+        ContentType: `image/${type}`,
         ContentEncoding: 'base64',
-        Key: 'posting/' + 'test',
-        Body: RegExp.$1,
+        Key: `posting/test/${new Date().toString()}${idx}.${type ?? 'png'}`,
+        Body: base64Data,
       };
       const data = await s3.upload(param).promise();
-      console.log(data);
-      content.replace(RegExp.$1, data.Location);
-    });
-    console.log(content);
+      result = result.replace(srcData, data.Location);
+      if (idx === srcList?.length) {
+        //todo : 백엔드로 요청 보내기
+      }
+    }
+    // srcList?.forEach(async (tag, idx) => {});
   };
   return (
     <PostingContainer>
