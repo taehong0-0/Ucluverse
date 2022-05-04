16a99b4ea9d33715beae861c4f0055696df8536f
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-05 01:24:13 +0900

    Feat/#7 : checkLogin 훅 수정

--- client/src/Hooks/useCheckLogin.ts
+++ client/src/Hooks/useCheckLogin.ts
@@ -1,30 +1,24 @@
 import axios from 'axios';
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
-import userState from '../Recoil/user';
+import userState from '../Recoil/User';
 
 const useCheckLogin = () => {
   const [user, setUser] = useRecoilState(userState);
+  const [status, setStatus] = useState(false);
   const checkLogin = async () => {
-    if (!user?.email) return false;
+    if (user.userIdx !== 0) return true;
     else {
-      const res = await axios.get(
-        `${process.env.REACT_APP_SERVER_URL}/auth/login?email=${user?.email}`,
-      );
-      return res.data;
-      // axios
-      //   .get(
-      //     `${process.env.REACT_APP_SERVER_URL}/auth/login?email=${user?.email}`,
-      //   )
-      //   .then((res) => {
-      //     if (res.data.status === 2) {
-      //       // setUser()
-      //       return true;
-      //     }
-      //     return false;
-      //   });
-      // return 'notLogin' 토큰 정보 없을 때
-      // return 'requireInfo' 토큰정보 있는데 유저정보 없을 때
-      // return 'login' 성공적으로 로그인 되었을 때
+      await axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/auth/islogin`)
+        .then((res) => {
+          if (res.data.status === 1) {
+            const { currentHashedRefreshToken, ...userData } = res.data.user;
+            setUser(userData);
+            setStatus(true);
+          }
+        });
+      return status;
     }
   };
   return checkLogin;
