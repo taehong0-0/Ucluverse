a9a35e9cb87d0a5ce90ff9d9b78cba76559eac17
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-06 02:34:24 +0900

    Feat/#7 : 로그인 상태관리 수정 recoil selector 추가 router 수정 비동기데이터 처리 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Navigate, Route, Routes, useHref } from 'react-router-dom';
 import Club from './Pages/Club/Club';
 import ClubList from './Pages/Club/ClubList';
@@ -16,53 +16,55 @@ axios.defaults.withCredentials = true;
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route
-          path="/login"
-          element={
-            <LoginRoute>
-              <Login />
-            </LoginRoute>
-          }
-        />
-        <Route
-          path="login/info"
-          element={
-            <LoginRoute>
-              <LoginInfo />
-            </LoginRoute>
-          }
-        />
-        <Route
-          path="/club/*"
-          element={
-            <AuthRoute>
-              <Club />
-            </AuthRoute>
-          }
-        />
-        <Route path="*" element={<Navigate replace to="/" />} />
+      <Suspense fallback={<span>로딩중</span>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/login"
+            element={
+              <LoginRoute>
+                <Login />
+              </LoginRoute>
+            }
+          />
+          <Route
+            path="login/info"
+            element={
+              <LoginRoute>
+                <LoginInfo />
+              </LoginRoute>
+            }
+          />
+          <Route
+            path="/club/*"
+            element={
+              <AuthRoute>
+                <Club />
+              </AuthRoute>
+            }
+          />
+          <Route path="*" element={<Navigate replace to="/" />} />
 
-        <Route
-          path="/clubList/*"
-          element={
-            <AuthRoute>
-              <ClubList />
-            </AuthRoute>
-          }
+          <Route
+            path="/clubList/*"
+            element={
+              <AuthRoute>
+                <ClubList />
+              </AuthRoute>
+            }
+          />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+        <img
+          src={banner}
+          style={{ width: '100vw', cursor: 'pointer' }}
+          onClick={() => {
+            history.pushState(null, '', '/');
+            window.location.replace('/');
+          }}
         />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-      <img
-        src={banner}
-        style={{ width: '100vw', cursor: 'pointer' }}
-        onClick={() => {
-          history.pushState(null, '', '/');
-          window.location.replace('/');
-        }}
-      />
-      <img src={footer} style={{ width: '100vw' }} />
+        <img src={footer} style={{ width: '100vw' }} />
+      </Suspense>
     </>
   );
 };
