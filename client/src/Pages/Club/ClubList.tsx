0f9747902fdcd8f20569ed0ba70058123323eaa1
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-05 17:26:43 +0900

    Feat/#138 : 소학회 목록 페이지 라우팅 추가

--- client/src/Pages/Club/ClubList.tsx
+++ client/src/Pages/Club/ClubList.tsx
@@ -1,12 +1,17 @@
 import React, { ReactElement } from 'react';
-import ClubBody from '../../Components/ClubList/ClubBody/ClubBody';
+import { Route, Routes } from 'react-router-dom';
+import ClubBody from '../../Components/ClubList/Body/ClubBody';
+import DepartmentBody from '../../Components/ClubList/Body/DepartmentClubBody';
 import Header from '../../Components/Header/Header';
 
 const ClubList = (): ReactElement => {
   return (
     <div>
       <Header />
-      <ClubBody />
+      <Routes>
+        <Route path="/" element={<ClubBody />} />
+        <Route path="/department" element={<DepartmentBody />} />
+      </Routes>
     </div>
   );
 };
