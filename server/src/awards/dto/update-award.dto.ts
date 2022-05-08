e4d6d43a4d56383968c0eef354d465dd4c3b0fb4
Kwondaehwi <kwondh8561@ajou.ac.kr>
2022-05-08 20:51:33 +0900

    Etc/#167 DTO에 apiProperty 추가

--- server/src/awards/dto/update-award.dto.ts
+++ server/src/awards/dto/update-award.dto.ts
@@ -1,16 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class UpdateAwardDto{
+    @ApiProperty({ description: '수상 인덱스' })
     @IsNumber()
     awardIdx: number
+    @ApiProperty({ description: '동아리 인덱스' })
     @IsNumber()
     clubIdx: number
+    @ApiProperty({ description: '대회 이름' })
     @IsString()
     competitionName: string;
+    @ApiProperty({ description: '상 이름' })
     @IsString()
     awardName: string;
+    @ApiProperty({ description: '내용' })
     @IsString()
     content: string;
+    @ApiProperty({ description: '사진 경로' })
     @IsString()
     path: string;
 }
--- server/src/likes/dto/create-like.dto.ts
+++ server/src/likes/dto/create-like.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber } from "class-validator";
 
 export class CreateLikeDto {
+    @ApiProperty({ description: '게시글 인덱스' })
     @IsNumber()
     postingIdx: number;
+    @ApiProperty({ description: '유저 인덱스' })
     @IsNumber()
     userIdx: number;
 }
