63cf240b7844ed298d70d2371bfb9ca566567065
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-13 16:10:29 +0900

    Test/#179 : 글 작성 페이지 수정

--- client/src/Components/Club/Posting/Posting.tsx
+++ client/src/Components/Club/Posting/Posting.tsx
@@ -5,16 +5,21 @@ import FloatInput from '../../Input/Input';
 import { PostingContainer } from './style';
 import { Buffer } from 'buffer';
 import AWS from 'aws-sdk';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../../Recoil/User';
+import { useContext } from 'react';
+import { ClubContext } from '../../../Pages/Club/Club';
+import { useLocation } from 'react-router-dom';
 
-interface props {
-  clubId: number;
-  boardIdx: Number;
-}
-const Posting = (props: props) => {
-  const { clubId, boardIdx } = props;
-  const titleRef = useRef(null);
+const Posting = () => {
+  const user = useRecoilValue(userState);
+  const { boardIdx, boardName } = window.history.state;
+  const titleRef = useRef<HTMLInputElement>(null);
   const [content, setContent] = useState<string>('');
+  const [imageList, setImageList] = useState<string[]>([]);
   const submit = async () => {
+    if (!titleRef.current || content === '') return;
     const option = {
       accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
@@ -46,12 +51,29 @@ const Posting = (props: props) => {
         .promise()
         .then((data) => {
           result = result.replace(srcData, data.Location);
+          setImageList((imageList) => [...imageList, data.Location]);
         });
     });
     await Promise.all(promiseList ?? []);
+    await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/postings/clubBoard/${boardIdx}`,
+      {
+        userIdx: user.userIdx,
+        title: titleRef.current.value,
+        content: content,
+        images: imageList,
+        allowComments: true,
+        isPublic: true,
+      },
+    );
   };
   return (
     <PostingContainer>
+      <div className="navigator">
+        <span>Home</span>
+        <span>{'>'}</span>
+        <span>{boardName}</span>
+      </div>
       <FloatInput inputRef={titleRef} name="제목" type="large" />
       <Editor setContent={setContent} />
       <Button name="작성" clickEvent={submit}></Button>
