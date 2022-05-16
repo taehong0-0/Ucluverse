fb3f4e2d1e7e203170047bee7bf031eaac49c13e
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-16 20:49:22 +0900

    Feat/#188 : 회원관리 컴포넌트 개발

---
+++ client/src/Components/Admin/ClubAdmin/Member/style.ts
@@ -0,0 +1,123 @@
+import styled from 'styled-components';
+
+export const AdminMemberContainer = styled.div`
+  width: 480px;
+  margin-left: 160px;
+  opacity: 1;
+  & > span {
+    font: normal normal bold 18px/24px Noto Sans KR;
+    letter-spacing: -1.35px;
+    color: #1a1917;
+    opacity: 1;
+  }
+  & > div:first-of-type {
+    margin-top: 12px;
+    background: #ffffff 0% 0% no-repeat padding-box;
+    opacity: 1;
+    height: 27px;
+    line-height: 27px;
+    border-top: 1px solid #707070;
+    border-bottom: 1px solid #707070;
+    opacity: 1;
+    span {
+      display: inline-block;
+      width: 100px;
+      font: normal normal bold 12px/16px Noto Sans KR;
+      letter-spacing: -0.6px;
+      color: #0e0e0e;
+      opacity: 1;
+    }
+  }
+  button {
+  }
+`;
+export const MemberBodyContainer = styled.div`
+  border-bottom: 2px solid #707070;
+  height: 455px;
+
+  & > div {
+    display: flex;
+    width: 480px;
+    height: 49px;
+    align-items: center;
+    & > span {
+      width: 100px;
+    }
+    div:first-child {
+      width: 100px;
+      select {
+        -webkit-appearance: none; /* for chrome */
+      }
+
+      select::-ms-expand {
+        display: none; /*for IE10,11*/
+      }
+      /* select[name='type'] {
+        display: inline-block;
+        width: 46px;
+        height: 19px;
+        background: #4e4e4e 0% 0% no-repeat padding-box;
+        box-shadow: 2px 2px 6px #00000029;
+        border-radius: 5px;
+        opacity: 1;
+        text-align: center;
+        font: normal normal normal 12px/20px Noto Sans KR;
+        letter-spacing: -0.9px;
+        color: #ffffff;
+      } */
+      select[title='임원'] {
+        display: inline-block;
+        width: 46px;
+        height: 19px;
+        background: #4e4e4e 0% 0% no-repeat padding-box;
+        box-shadow: 2px 2px 6px #00000029;
+        border-radius: 5px;
+        opacity: 1;
+        text-align: center;
+        font: normal normal normal 12px/20px Noto Sans KR;
+        letter-spacing: -0.9px;
+        color: #ffffff;
+      }
+      select[title='회원'] {
+        display: inline-block;
+        width: 46px;
+        height: 19px;
+        /* UI Properties */
+        background: #f5f5f5 0% 0% no-repeat padding-box;
+        box-shadow: 2px 2px 6px #00000029;
+        border-radius: 5px;
+        opacity: 1;
+        text-align: center;
+        font: normal normal normal 12px/20px Noto Sans KR;
+        letter-spacing: -0.9px;
+        color: #4e4e4e;
+      }
+      /* .executives {
+        display: inline-block;
+        width: 46px;
+        height: 19px;
+        background: #4e4e4e 0% 0% no-repeat padding-box;
+        box-shadow: 2px 2px 6px #00000029;
+        border-radius: 5px;
+        opacity: 1;
+        text-align: center;
+        font: normal normal normal 12px/20px Noto Sans KR;
+        letter-spacing: -0.9px;
+        color: #ffffff;
+      }
+      .normal {
+        display: inline-block;
+        width: 46px;
+        height: 19px;
+        background: #f5f5f5 0% 0% no-repeat padding-box;
+        box-shadow: 2px 2px 6px #00000029;
+        border-radius: 5px;
+        opacity: 1;
+        text-align: center;
+        font: normal normal normal 12px/20px Noto Sans KR;
+        letter-spacing: -0.9px;
+        color: #4e4e4e;
+      }*/
+    }
+  }
+`;
