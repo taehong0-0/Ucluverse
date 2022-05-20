483c95af3dba5f260d670ffcf78d573ae38ed90d
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-20 17:30:19 +0900

    Feat/#202 : 동아리 목록 추가

--- client/src/Pages/MyPage/MyPage.tsx
+++ client/src/Pages/MyPage/MyPage.tsx
@@ -1,15 +1,15 @@
 import React, { ReactElement } from 'react';
 import Header from '../../Components/Header/Header';
+import MyClub from '../../Components/MyPage/MyClub/MyClub';
 import MyInfo from '../../Components/MyPage/MyInfo/MyInfo';
 
 const MyPage = (): ReactElement => {
-  //useCheckLogin();
-  //design test
   return (
     <div>
       <Header />
       <div>
         <MyInfo />
+        <MyClub />
       </div>
     </div>
   );
