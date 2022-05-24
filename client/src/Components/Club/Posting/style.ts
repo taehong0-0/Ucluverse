0f7c089f4a414454e838ac83cdf0fdb738391f0f
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-05-25 01:03:05 +0900

    Feat/#179 : 게시글 작성 에디터 스타일 수정

--- client/src/Components/Club/Posting/style.ts
+++ client/src/Components/Club/Posting/style.ts
@@ -2,12 +2,15 @@ import styled from 'styled-components';
 import { BoardStyle } from '../Board/style';
 
 export const PostingContainer = styled.div`
-  ${BoardStyle}
-  &>div:nth-of-type(2) {
-    margin-left: 1.25rem;
+  width: 56.3%;
+  & > div {
+    ${BoardStyle}
+    &>div:nth-of-type(2) {
+      margin-left: 1.25rem;
+    }
   }
   button {
     margin-top: 0.625rem;
-    margin-left: 52vw;
+    margin-left: 95%;
   }
 `;
