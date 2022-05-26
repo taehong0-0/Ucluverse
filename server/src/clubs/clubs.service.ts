daabd2aab0161c2071c5ef99932d8e704e962bf8
Kwondaehwi <kwondh8561@ajou.ac.kr>
2022-05-26 23:18:02 +0900

    과소학회 get 수정

--- server/src/clubs/clubs.service.ts
+++ server/src/clubs/clubs.service.ts
@@ -144,7 +144,7 @@ export class ClubsService {
                 .addSelect(['cbs.clubBoardIdx', 'cbs.name'])
                 .leftJoin('club.clubCategories' , 'ccs')
                 .leftJoin('club.clubBoards', 'cbs')
-                .where('club.clubType = :clubType', { clubType: '과소학회' })
+                .where('club.clubType = :clubType', { clubType: 'departmentClub' })
                 .getMany();
             
             departmentClubs.forEach(departmentClub => {
